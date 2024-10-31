87e7f8d964b1e058b886fa7055e84273826cebd2
MGHMSJ5 <elise.koezema@hotmail.com>
2024-10-31 21:34:29 +0100

    Catching
    Added sprite folder and added a smooth-corner square.
    Also changed the catching script.
    Changed the SampleScene by putting in the UI and script

--- Microplastic Project/Assets/Scripts/Microplastic related/Catching.cs
+++ Microplastic Project/Assets/Scripts/Microplastic related/Catching.cs
@@ -1,26 +1,71 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Catching : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private List<GameObject> _catchableObjects = new List<GameObject>();
+    [SerializeField]
+    private GameObject _catchButton;
+
+    private Button _button;
+    private Image _image;
+    [SerializeField]
+    private Color _canUseColor;
+    [SerializeField]
+    private Color _cantUseColor;
+
+    void Awake()
     {
-        
+        _button = _catchButton.GetComponent<Button>();
+        _image = _catchButton.GetComponent<Image>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        CanCatch(false);
+    }
+
+    public void CatchMicroplastic()
+    {
+        //for nw destory the parent of the parent (because of the way the movement works for now)
+        Destroy(_catchableObjects[0].transform.parent.parent.gameObject);
+        _catchableObjects.RemoveAt(0);
+
+        CatchCheck();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "MP1")
         {
-            print("in trigger");
+            //add to list so that it can handle multiple microplastics
+            _catchableObjects.Add(other.gameObject);
+            CanCatch(true);
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "MP1")
+        {
+            _catchableObjects.Remove(other.gameObject);
+        }
+
+        CatchCheck();
+    }
+    private void CatchCheck()
+    {
+        if (_catchableObjects.Count <= 0)
+        {
+            CanCatch(false);
+        }
+    }
+
+    private void CanCatch(bool can)
+    {
+        _button.enabled = can;
+        _image.color = can ? _canUseColor : _cantUseColor;
     }
 }
