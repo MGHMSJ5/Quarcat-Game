4a58889d8d88bf714f897f57d048ef2592e59e21
MGHMSJ5 <elise.koezema@hotmail.com>
2024-10-31 15:03:37 +0100

    Spawning Update
    Changed the SpawningManager so that there is a limit of microplastics that spawn, and that they don't spawn again when re-entering the collider.
    Also a scene change where the variable of SpawnNumber is added

--- Microplastic Project/Assets/Scripts/Microplastic creatures/SpawningManager.cs
+++ Microplastic Project/Assets/Scripts/Microplastic creatures/SpawningManager.cs
@@ -6,12 +6,38 @@ public class SpawningManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject _spawningGameObject;
+    [SerializeField]
+    private int _spawnNumber = 4;
+
+    private float _spawnBreakMax = 2f;
+    private float _spawnBreakMin = 1f;
+
+    private bool _canSpawn = true; //added bool so that spawning only happens once
+    
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (_canSpawn)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                StartCoroutine(SpawnMicroplastics());
+                _canSpawn = false;
+            }
+        }
+    }
+
+    IEnumerator SpawnMicroplastics()
+    {
+        for (int i = 0; i < _spawnNumber; i++)
         {
             Instantiate(_spawningGameObject, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(GetRandomTime(_spawnBreakMax, _spawnBreakMin));
         }
+    }
+
+    private float GetRandomTime(float maxTime, float minTime)
+    {
+        return Random.Range(maxTime, minTime);
     }
 }
