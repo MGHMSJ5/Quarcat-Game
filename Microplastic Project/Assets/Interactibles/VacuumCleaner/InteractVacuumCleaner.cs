fbcf2c8c128a871c447079885b558c2f3474865e
MGHMSJ5 <elise.koezema@hotmail.com>
2024-12-01 14:53:23 +0100

    Added prefabs and script
    Added a script for the Vacuum cleaner to interact with it: InteractVacuumCleaner.
    Added some folders in the Assets. 1 in Interactibles: VacuumCleaner. And 1 in the Assets itself: Vacuum Cleaner. I added a prefab for the interactible vacuum cleaner, THE vacuum cleaner that'll follow you, and 1 that contains the greybox model of the vacuum cleaner

---
+++ Microplastic Project/Assets/Interactibles/VacuumCleaner/InteractVacuumCleaner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BasicInteractLayout))]
+public class InteractVacuumCleaner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _vacuumPrefab;
+
+    private BasicInteractLayout _basicInteractLayout;
+
+    void Awake()
+    {
+        _basicInteractLayout = GetComponent<BasicInteractLayout>();
+        _basicInteractLayout._interactAction += PickUpVacuumCleaner; //Depends on function name
+    }
+
+    //unique function/interaction based on script/object
+    void PickUpVacuumCleaner()
+    {
+        //put here the things that need to happen before detroying this interactible
+        if (_vacuumPrefab != null)
+        {
+            Instantiate(_vacuumPrefab, _vacuumPrefab.transform);
+        }
+
+        GameObject parent = this.gameObject.transform.parent.gameObject;
+        Destroy(parent);
+
+        //_basicInteractLayout.ExitANDNotInteract(true); //in case you want to only interact once
+    }
+}
