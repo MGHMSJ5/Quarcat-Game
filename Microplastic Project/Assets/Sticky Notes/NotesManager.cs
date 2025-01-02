d59d2f1ca2ad6b7071f4920e3510be0aeffb1196
MGHMSJ5 <elise.koezema@hotmail.com>
2025-01-02 16:08:03 +0100

    Tiny code cleanup

--- Microplastic Project/Assets/Sticky Notes/NotesManager.cs
+++ Microplastic Project/Assets/Sticky Notes/NotesManager.cs
@@ -1,6 +1,4 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public static class NotesManager
 {
@@ -29,7 +27,6 @@ public static class NotesManager
                 return true;
             }
         }
-
         return false;
     }
 }
