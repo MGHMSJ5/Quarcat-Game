0088a2dfbb252ac82e5c748567ab3757b5ce767f
Capuwubara <95750224+Capuwubara@users.noreply.github.com>
2025-01-05 01:10:33 +0100

    Added interactive buttons for options and credits
    Buttons for options now go to a slider for sound (it doesn't work yet, I mean there''s no sound in the game right now. but shouldn't be difficult to add)
    Button for credit goes to a list of out names
    Back buttons go back to main menu

--- Microplastic Project/Assets/MainMenu/MainMenu.cs
+++ Microplastic Project/Assets/MainMenu/MainMenu.cs
@@ -5,15 +5,48 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("UI Panels")]
+    public GameObject mainMenuPanel;    // The main menu UI
+    public GameObject optionsPanel;    // The options menu UI
+    public GameObject creditsPanel;    // The credits menu UI
+
     public void PlayGame()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(0); 
     }
+
     public void QuitGame()
     {
         Debug.Log("Game Quit");
-
-        Application.Quit();
+        Application.Quit(); 
     }
 
+    public void OpenOptions()
+    {
+        // Show the options menu and hide the main menu
+        mainMenuPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+
+    public void OpenCredits()
+    {
+        // Show the credits menu and hide the main menu
+        mainMenuPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+
+    public void BackToMainMenu()
+    {
+        // Hide both the options and credits menus, show the main menu
+        optionsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void AdjustSound(float value)
+    {
+        // Adjust the global sound volume
+        Debug.Log("Sound adjusted to: " + value);
+        AudioListener.volume = value;
+    }
 }
